bf55c56f7daa146db378bd276e1fa43fb6aceac4
patelvk2000 <patelvk2000@gmail.com>
2024-04-15 06:20:38 +0000

    change the signin

--- src/components/auth-components.tsx
+++ src/components/auth-components.tsx
@@ -1,28 +1,19 @@
+'use client';
+import { signIn, signOut } from 'next-auth/react';
 import { Button } from './ui/button';
-import { signIn, signOut } from '@/app/auth';
 
 export function SignIn({ provider, ...props }: { provider?: string } & React.ComponentPropsWithRef<typeof Button>) {
   return (
-    <form
-      action={async () => {
-        'use server';
-        await signIn(provider);
-      }}
-    >
-      <Button {...props}>Sign In</Button>
-    </form>
+    <Button {...props} onClick={() => signIn(provider)}>
+      Sign In
+    </Button>
   );
 }
 
 export function SignOut(props: React.ComponentPropsWithRef<typeof Button>) {
   return (
-    <form
-      action={async () => {
-        'use server';
-        await signOut();
-      }}
-    >
-      <Button {...props}>Sign out</Button>
-    </form>
+    <Button onClick={() => signOut()} {...props}>
+      Sign out
+    </Button>
   );
 }
