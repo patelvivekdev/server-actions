769f73c31ce0f05a29213929fed701b5697cd936
patelvk2000 <patelvk2000@gmail.com>
2024-04-16 03:56:48 +0000

    add version 5.0.0-beta.16 to next-auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import Navbar from '@/components/Navbar';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Server Actions',
+  title: 'Server Actions by patelvivek.dev',
   description: 'Learn server actions with patelvivek.dev',
 };
 
