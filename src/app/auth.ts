f7045ed71c2116f1c6c0c66306a651ccb9da3add
VIVEK PATEL <patelvk2000@gmail.com>
2024-04-16 02:49:04 -0400

    Remove the callback to check.

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -9,18 +9,7 @@ export const config = {
   adapter: SupabaseAdapter({
     url: process.env.SUPABASE_URL as string,
     secret: process.env.SUPABASE_SERVICE_ROLE_KEY as string,
-  }),
-  callbacks: {
-    authorized({ request, auth }) {
-      const { pathname } = request.nextUrl;
-      if (pathname === '/middleware-example') return !!auth;
-      return true;
-    },
-    jwt({ token, trigger, session }) {
-      if (trigger === 'update') token.name = session.user.name;
-      return token;
-    },
-  },
+  })
 } satisfies NextAuthConfig;
 
 export const { handlers, auth, signIn, signOut } = NextAuth(config);
