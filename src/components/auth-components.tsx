843d148568a4e315ca915000fc08a9f51577de59
VIVEK PATEL <patelvk2000@gmail.com>
2024-04-18 17:03:01 -0400

    Update version of nextjs to latest.

--- src/components/auth-components.tsx
+++ src/components/auth-components.tsx
@@ -1,19 +1,32 @@
-'use client';
-import { signIn, signOut } from 'next-auth/react';
+// import { signIn, signOut } from 'next-auth/react';
 import { Button } from './ui/button';
+import { signIn, signOut } from '@/app/auth';
 
 export function SignIn({ provider, ...props }: { provider?: string } & React.ComponentPropsWithRef<typeof Button>) {
   return (
-    <Button {...props} onClick={() => signIn(provider)}>
-      Sign In
-    </Button>
+    <form
+      action={async () => {
+        'use server';
+        await signIn(provider);
+      }}
+    >
+      <Button {...props}>Sign In</Button>
+    </form>
   );
 }
 
 export function SignOut(props: React.ComponentPropsWithRef<typeof Button>) {
   return (
-    <Button onClick={() => signOut()} {...props}>
-      Sign out
-    </Button>
+    <form
+      action={async () => {
+        'use server';
+        await signOut();
+      }}
+      className='w-full'
+    >
+      <Button variant='ghost' className='w-full p-0' {...props}>
+        Sign Out
+      </Button>
+    </form>
   );
 }
